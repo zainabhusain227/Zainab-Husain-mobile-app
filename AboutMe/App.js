c4300ef45dd0b2a1802985feba47b6259b07470b
zainabhusain227 <41973371+zainabhusain227@users.noreply.github.com>
2020-02-23 02:12:01 -0500

    adding image and app.js changed

--- AboutMe/App.js
+++ AboutMe/App.js
@@ -1,20 +1,40 @@
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import {Image, Alert,Button, Platform, StyleSheet, Text, View } from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android: 'Double tap R on your keyboard to reload,\n' + 'Shake or press menu button for dev menu',
 });
+import me from './assets/me.jpg'; 
+import { apisAreAvailable } from 'expo';
+
 
 export default class App extends Component {
+
   render() {
+
+    Alert.alert('Welcome', 'to Pigpocket App!',[
+      {text:'okay',onPress:() => console.log('alert closed')}
+    ])
+
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Text style={styles.welcome}>Hi, I'm Zainab!</Text>
+        <Image source={me} style={{resizeMode: 'cover', width: 305, height: 159 }} /> 
+        <Text style={styles.instructions}>I'm a Junior at McMaster University. My hobbies include reading, painting and playing my ukelele. I also love travelling so I'm super excited to learn some financial tips if given the chance to work at Pigpocket!
+         </Text>
+         <Button
+          color="#FFC980"
+          title="Press me"
+          onPress={() => Alert.alert('Thanks for pressing!')}
+        />
       </View>
     );
+    
+
+    
+
+
   }
 }
 
@@ -23,10 +43,10 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#FFEFDF',
   },
   welcome: {
-    fontSize: 20,
+    fontSize: 30,
     textAlign: 'center',
     margin: 10,
   },
